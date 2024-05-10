77fdad89510d90df4ce88c2e7434ba214b66c1a2
Fernanda-Barreto <fernnda1bf@gmail.com>
2024-05-10 20:52:34 -0300

    graficos

--- src/charts/barChart.js
+++ src/charts/barChart.js
@@ -1,6 +1,8 @@
 export const barChart = (data) => {
   const db = data;
-  console.log("aquiiii", db);
+
+  // Array para armazenar todos os tipos de dados conhecidos
+  const allKinds = ['leque', 'panfleto', 'cartaz', 'adesivo', 'encarte', 'cartilha', 'outro'];
 
   // Objeto para armazenar a soma dos valores por kind e suas cores correspondentes
   const kindData = {};
@@ -9,19 +11,15 @@ export const barChart = (data) => {
   // Gerando uma cor única para cada kind
   const uniqueColors = ['#FF5733', '#33FF57', '#5733FF', '#33FFC7', '#FF33B2', '#FFFF33', '#33FFEC', '#3387FF', '#FF3394', '#33FF9E'];
 
-  // Tipos conhecidos de kind
-  const knownKinds = ['leque', 'panfleto', 'cartaz', 'adesivo', 'encarte', 'cartilha'];
-
   // Calculando a soma dos valores por kind e atribuindo cores
-  db.forEach((item, index) => {
-    let kind = knownKinds.includes(item.kind.toLowerCase()) ? item.kind.toLowerCase() : 'outro';
-
-    if (!kindData[kind]) {
-      kindData[kind] = item.quantity;
-      colorsMap[kind] = uniqueColors[index % uniqueColors.length]; // Usando cores únicas para cada kind
+  allKinds.forEach((kind, index) => {
+    let found = db.find(item => item.kind.toLowerCase() === kind);
+    if (found) {
+      kindData[kind] = found.quantity;
     } else {
-      kindData[kind] += item.quantity;
+      kindData[kind] = 0; // Definindo o valor como 0 se não houver dados para essa categoria
     }
+    colorsMap[kind] = uniqueColors[index % uniqueColors.length]; // Usando cores únicas para cada kind
   });
 
   // Convertendo o objeto de soma para o formato esperado pela biblioteca de gráficos
@@ -52,10 +50,20 @@ export const barChart = (data) => {
     dataLabels: {
       enabled: false
     },
+    xaxis: {
+      categories: allKinds, // Mostrando todas as categorias no eixo x
+      labels: {
+        rotate: -45, // Rotacionando os rótulos para melhor visualização
+        offsetY: -5,
+        style: {
+          fontSize: '12px'
+        }
+      }
+    },
     legend: {
       show: true,
       showForSingleSeries: true,
-      customLegendItems: ['Actual', 'Expected'],
+      customLegendItems: allKinds, // Usando todas as categorias na legenda
       markers: {
         fillColors: uniqueColors // Usando as cores únicas para os marcadores da legenda
       }
